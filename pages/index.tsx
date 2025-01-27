f9264e8306d5c9f34a7dd89e37de99378a0e6bd1
laurasfelix <laurasfelix@hotmail.com.br>
2025-01-27 15:59:01 -0800

    quick fix eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
