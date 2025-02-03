9dac5e56c63d9d0ced6e1cd92f427f8ecc0f58da
laurasfelix <laurasfelix@hotmail.com.br>
2025-02-03 00:35:58 -0800

    disable eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
+/* eslint-disable */
 import Questionnaire from "@/components/questionnaire";
 import Image from "next/image";
-import { useRouter } from "next/router";
 import { useState } from "react";
 import { Timezone } from "@/types/timezone";
 
 export default function Home() {
-  const router = useRouter();
   const [submitted, setSubmitted] = useState(false);
 
 
